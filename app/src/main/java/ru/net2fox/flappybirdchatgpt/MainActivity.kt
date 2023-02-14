fc682679d7b06dec4b02a044776ac33089e17c1c
Net2Fox <trakounet.fox@gmail.com>
2023-02-14 23:22:15 +0300

    Hiding the navigation bar

--- app/src/main/java/ru/net2fox/flappybirdchatgpt/MainActivity.kt
+++ app/src/main/java/ru/net2fox/flappybirdchatgpt/MainActivity.kt
@@ -2,11 +2,19 @@ package ru.net2fox.flappybirdchatgpt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
     }
 }
 
